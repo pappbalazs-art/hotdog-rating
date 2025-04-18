05fefb0225d5e72f09d2d3bbd458c8a40be29383
Papp Balázs <hello@pappbalazs.com>
2025-04-18 02:13:44 +0200

    Added Firebase to the app.

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+import { firebaseConfig } from "./firebase.config";
+
+const app = initializeApp(firebaseConfig);
+
+export const database = getFirestore(app);
+
+export default app;
